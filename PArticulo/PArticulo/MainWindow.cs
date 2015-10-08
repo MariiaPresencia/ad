7ef512e6274c14af995d1f9b1fd10061b73cd25f
MariiaPresencia <mariia1993@hotmail.es>
2015-10-08 08:47:41 +0200

    PArticulo: variables de tipo interfaces

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using Gtk;
+using System.Data;
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Data.Common;
@@ -11,19 +12,19 @@ public partial class MainWindow: Gtk.Window
 		Build ();
 
 		Console.WriteLine ("MainWindow ctor.");
-		MySqlConnection mySqlConnection = new MySqlConnection (
+		IDbConnection dbConnection = new MySqlConnection (
 			"Database=dbprueba;Data Source=localhost;User Id=root;Password=sistemas"
 		);
-		mySqlConnection.Open ();
+		dbConnection.Open ();
 		
-		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
-		mySqlCommand.CommandText = "select * from articulo";
-		MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = "select * from articulo";
+		IDataReader dataReader = dbCommand.ExecuteReader ();
 
 
 
 		//extraer columnas con el metodo getColumNames de abajo
-		string[] columnas = getColumnNames (mySqlDataReader);
+		string[] columnas = getColumnNames (dataReader);
 
 		//añadimos columnas
 		for (int i = 0; i < columnas.Length; i ++) {
@@ -34,24 +35,24 @@ public partial class MainWindow: Gtk.Window
 		//treeView.AppendColumn ("nombre", new CellRendererText (), "text" , 1);
 
 		//establezco el modelo
-		Type[] types = getTypes (mySqlDataReader.FieldCount);
+		Type[] types = getTypes (dataReader.FieldCount);
 		ListStore listStore = new ListStore (types);
 		
 		//mostramos los datos de la BD en el treeView
-			while (mySqlDataReader.Read()) {
+			while (dataReader.Read()) {
 				//extraemos los valies con el metodo getValues de abajo
-				string[] values = getValues (mySqlDataReader);
+				string[] values = getValues (dataReader);
 				listStore.AppendValues (values);
 				//en el caso de que solo fuera id y nombre y sin el for
-				//listStore.AppendValues(mySqlDataReader[0].toString(),mySqlDataReader[1].toString());
+				//listStore.AppendValues(dataReader[0].toString(),dataReader[1].toString());
 			}
 
 
-		mySqlDataReader.Close ();
+		dataReader.Close ();
 
 		treeView.Model = listStore;
 
-		mySqlConnection.Close ();
+		dbConnection.Close ();
 
 //		//añadimos columnas
 //		
@@ -70,11 +71,11 @@ public partial class MainWindow: Gtk.Window
 
 	}
 	//metodo que devuelve el nombre de las columnas
-	private string[] getColumnNames(MySqlDataReader mySqlDataReader) {
-			int count = mySqlDataReader.FieldCount;
+	private string[] getColumnNames(IDataReader dataReader) {
+			int count = dataReader.FieldCount;
 			List<string> columNames = new List<string> ();
 			for (int index = 0; index < count; index++)
-				columNames.Add (mySqlDataReader.GetName (index));
+				columNames.Add (dataReader.GetName (index));
 			return columNames.ToArray ();
 		}
 
@@ -88,11 +89,11 @@ public partial class MainWindow: Gtk.Window
 	}
 
 
-	private string[] getValues(MySqlDataReader mySqlDataReader){
+	private string[] getValues(IDataReader dataReader){
 		List<string> values = new List<string> ();
-		int count = mySqlDataReader.FieldCount;
+		int count = dataReader.FieldCount;
 		for (int i = 0; i < count; i++) {
-			values.Add(mySqlDataReader[i].ToString());
+			values.Add(dataReader[i].ToString());
 		}
 		return values.ToArray ();
 	}
--- PArticulo/PArticulo/gtk-gui/MainWindow.cs
+++ PArticulo/PArticulo/gtk-gui/MainWindow.cs
@@ -4,7 +4,6 @@ public partial class MainWindow
 {
 	private global::Gtk.UIManager UIManager;
 	private global::Gtk.Action refreshAction;
-	private global::Gtk.Action refreshAction1;
 	private global::Gtk.VBox vbox1;
 	private global::Gtk.VBox vbox2;
 	private global::Gtk.Toolbar toolbar;
@@ -19,8 +18,6 @@ public partial class MainWindow
 		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
 		this.refreshAction = new global::Gtk.Action ("refreshAction", null, null, "gtk-refresh");
 		w1.Add (this.refreshAction, null);
-		this.refreshAction1 = new global::Gtk.Action ("refreshAction1", null, null, "gtk-refresh");
-		w1.Add (this.refreshAction1, null);
 		this.UIManager.InsertActionGroup (w1, 0);
 		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
@@ -35,7 +32,7 @@ public partial class MainWindow
 		this.vbox2.Name = "vbox2";
 		this.vbox2.Spacing = 6;
 		// Container child vbox2.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar'><toolitem name='refreshAction1' action='refreshAction1'/></toolbar></ui>");
+		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar'><toolitem name='refreshAction' action='refreshAction'/></toolbar></ui>");
 		this.toolbar = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar")));
 		this.toolbar.Name = "toolbar";
 		this.toolbar.ShowArrow = false;
