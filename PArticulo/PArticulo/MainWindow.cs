9b6ab7ebb89316dc9b372834eaa8a08c8018963f
MariiaPresencia <mariia1993@hotmail.es>
2015-10-15 10:28:31 +0200

    PArticulo:añadiendo clase TreeViewHelper

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -1,9 +1,5 @@
 using System;
 using Gtk;
-using System.Data;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data.Common;
 
 using PArticulo;
 
@@ -16,28 +12,7 @@ public partial class MainWindow: Gtk.Window
 		Console.WriteLine ("MainWindow ctor.");
 
 		QueryResult queryResult = PersisterHelper.Get ("select * from articulo");
-
-		//extraer columnas 
-		string[] columnas = queryResult.ColumnNames;
-		CellRendererText cellRendererText = new CellRendererText ();
-		//añadimos columnas
-		for (int i = 0; i < columnas.Length; i ++) {
-			//otra manera de añadir columnas con un delegate
-			int column = i;
-			treeView.AppendColumn (columnas [i], cellRendererText,
-			       delegate(TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-				IList row = (IList)tree_model.GetValue(iter,0);
-				cellRendererText.Text = row[column].ToString();
-			});
-			treeView.AppendColumn (columnas[i].ToString(), new CellRendererText (), "text", i);
-		}
-
-		ListStore listStore = new ListStore (typeof(IList));
-		foreach (IList row in queryResult.Rows) {
-			listStore.AppendValues (row);
-		}
-		treeView.Model = listStore;
-
+		TreeViewHelper.Fill (treeView, queryResult);
 	}
 
 
---
+++ PArticulo/PArticulo/TreeViewHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using Gtk;
+using System.Collections;
+
+namespace PArticulo
+{
+	public class TreeViewHelper
+	{
+		public static void Fill(TreeView treeView , QueryResult queryResult){
+			string[] columnas = queryResult.ColumnNames;
+			CellRendererText cellRendererText = new CellRendererText ();
+			//añadimos columnas
+			for (int i = 0; i < columnas.Length; i ++) {
+				//otra manera de añadir columnas con un delegate
+				int column = i;
+				treeView.AppendColumn (columnas [i], cellRendererText,
+				                       delegate(TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+					IList row = (IList)tree_model.GetValue(iter,0);
+					cellRendererText.Text = row[column].ToString();
+				});
+			}
+
+			ListStore listStore = new ListStore (typeof(IList));
+			foreach (IList row in queryResult.Rows) {
+				listStore.AppendValues (row);
+			}
+			treeView.Model = listStore;
+		}
+
+	}
+}
+
