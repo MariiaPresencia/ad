2f9d3e66e219075efaf8ddf5770f1d04315e95bf
MariiaPresencia <mariia1993@hotmail.es>
2015-11-03 09:01:02 +0100

    PArticulo:Añadiendo ComboBoxHelper

--- PArticulo/PArticulo/ArticuloView.cs
+++ PArticulo/PArticulo/ArticuloView.cs
@@ -13,18 +13,19 @@ namespace PArticulo
 			this.Build ();
 //			entryNombre.Text = "nuevo";
 			QueryResult queryResult = PersisterHelper.Get ("select * from categoria");
-			CellRendererText cellRendererText = new CellRendererText ();
-			comboBoxCategoria.PackStart (cellRendererText, false);
-			comboBoxCategoria.SetCellDataFunc (cellRendererText,
-			       delegate(CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-				IList row = (IList)tree_model.GetValue(iter, 0);
-				cellRendererText.Text = row[1].ToString();
-
-			});
-			ListStore listStore = new ListStore (typeof(IList));
-			foreach (IList row in queryResult.Rows)
-				listStore.AppendValues (row);
-			comboBoxCategoria.Model = listStore;
+			ComboBoxHelper.Fill (comboBoxCategoria, queryResult);
+//			CellRendererText cellRendererText = new CellRendererText ();
+//			comboBoxCategoria.PackStart (cellRendererText, false);
+//			comboBoxCategoria.SetCellDataFunc (cellRendererText,
+//			       delegate(CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+//				IList row = (IList)tree_model.GetValue(iter, 0);
+//				cellRendererText.Text = row[1].ToString();
+//
+//			});
+//			ListStore listStore = new ListStore (typeof(IList));
+//			foreach (IList row in queryResult.Rows)
+//				listStore.AppendValues (row);
+//			comboBoxCategoria.Model = listStore;
 
 		}
 //			spinButtonPrecio.Value = 1.5;
