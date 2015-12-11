75a63ec31bb847f08005b8be48dd535f27e8e7c7
MariiaPresencia <mariia1993@hotmail.es>
2015-12-11 19:00:14 +0100

    Añadiendo GHolaMundo

---
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/HolaMundo.java
@@ -0,0 +1,10 @@
+package org.institutoserpis.ad;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo desde eclipse con Gradle");
+	}
+
+}
