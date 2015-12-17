8c998f468b26ee4c2ed3b4849fff992db607a6a1
MariiaPresencia <mariia1993@hotmail.es>
2015-12-17 10:34:18 +0100

    GMysql: Resuelto el insert , update y delete

--- GMysql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMysql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -6,39 +6,79 @@ import java.sql.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import com.mysql.jdbc.PreparedStatement;
 
 public class PruebaArticulo {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		//conectar con la base de datos 
 		Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
-		
 		Statement statement = connection.createStatement();
+		
+		//preparando el insert, update y delete
+		String insert="INSERT INTO articulo VALUES(null,?,?,?)";
+		String update = "UPDATE articulo SET nombre = ? WHERE categoria=? AND precio = ?";
+		String delete = "DELETE FROM articulo WHERE nombre = ?";
+		
+		//los creamos y ejecutamos
+		PreparedStatement insertar = (PreparedStatement)connection.prepareStatement(insert);
+		insertarDatos(insertar);
+		PreparedStatement update1 = (PreparedStatement) connection.prepareStatement(update);
+		updateDatos(update1);
+		PreparedStatement delete1 = (PreparedStatement) connection.prepareStatement(delete);
+		deleteDatos(delete1);
+		
 		//ejecutamos la sentencia select * from articulo
 		ResultSet result = statement.executeQuery("SELECT * FROM articulo");
 		
-		ResultSetMetaData rsMetaData = result.getMetaData();
+		//mostramos la tabla con los datos
+		System.out.println("TABLA:");
+		mostrarDatos(result);
+		System.out.println("\n");
+		
+		result.close();
+		statement.close();
+		connection.close();
+		System.out.println("Fin");
+	}
+	
+	private static int insertarDatos(PreparedStatement insertar) throws SQLException{
+		insertar.setString(1, "articuloInsertar3");
+		insertar.setInt(2, 3);
+		insertar.setDouble(3, 5.7);
+		return insertar.executeUpdate();
+	}
+	
+	private static int updateDatos(PreparedStatement insertar) throws SQLException{
+		insertar.setString(1, "articulo7");
+		insertar.setInt(2, 1);
+		insertar.setDouble(3, 5.7);
+		return insertar.executeUpdate();
+	}
+	
+	private static int deleteDatos(PreparedStatement insertar)throws SQLException{
+		insertar.setString(1, "articulo 23");
+		return insertar.executeUpdate();
+	}
+	
+	private static void mostrarDatos(ResultSet rs) throws Exception {	
+		ResultSetMetaData rsMetaData = rs.getMetaData();
 		int numColum = rsMetaData.getColumnCount();
-		System.out.println("Nombre de las columnas:");
+		System.out.println("Nombre de las columnas: ");
 		for (int i =1; i < numColum+1; i++) {
 		    //mostamos los nombres de las columnas
 			String columnName = rsMetaData.getColumnName(i);
-		    System.out.print(columnName+"|");
-		    
+		    System.out.print(columnName+ "         "+"\t");
 		}
 		System.out.println();
-		System.out.println();
-		while(result.next()){
-			String id1 = result.getString(1);
-		    String nombre1 = result.getString(2);
-		    String categoria1= result.getString(3);
-		    String precio1 = result.getString(4);
-		    //mostramos los datos de las columnas de la base de datos
-		    System.out.println(id1+" | "+nombre1+" | "+ categoria1 +" | "+ precio1 +"\n");
-		}
-
-		connection.close();
-		System.out.println();
-		System.out.println("Fin");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("\n");
+		while (rs.next()) {
+		      for (int i = 1; i < numColum + 1; i++) {
+		        System.out.print(rs.getString(i) + "         "+"\t");
+		      }
+		      System.out.println();
+		    }
 	}
+		
 }
