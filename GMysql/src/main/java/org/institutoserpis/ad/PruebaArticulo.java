c3bced175862d6d695d2b1c3bc9a1e246bd1c6d9
MariiaPresencia <mariia1993@hotmail.es>
2016-01-07 10:42:39 +0100

    PruebaArticulo : falta leer y modificar el editar

--- GMysql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMysql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class PruebaArticulo {
 	static Scanner teclado = new Scanner(System.in);
@@ -20,81 +19,80 @@ public class PruebaArticulo {
 		
 		//ejecutamos la sentencia select * from articulo
 		ResultSet result = statement.executeQuery("SELECT * FROM articulo");
-		
-		System.out.println( "                 "+"\tTABLA:");
 		mostrarDatos(result);
-		System.out.println("\n");
-		
+		int num;
+		do{
 		//creamos el menu y ejecutamos los metodos correspondientes
-		System.out.println("MENU:");
-		System.out.println("1- Leer");
-		System.out.println("2- Nuevo");
-		System.out.println("3- Editar");
-		System.out.println("4- Eliminar");
-		System.out.println("");
-		System.out.print("Opcion que eliges:");
-		int num = teclado.nextInt();
+			System.out.println("MENU:");
+			System.out.println("1- Leer");
+			System.out.println("2- Nuevo");
+			System.out.println("3- Editar");
+			System.out.println("4- Eliminar");
+			System.out.println("5- Listar todos");
+			System.out.println("0- Salir");
+			System.out.println("");
+			System.out.print("Opcion que eliges:");
+			num = teclado.nextInt();
 		
+			
 		switch(num){
 		case 1:
-			
+			String visualizar = "SELECT id FROM articulo";
+			ResultSet vis = statement.executeQuery(visualizar);
 			break;
 		case 2: 
-			String insert="INSERT INTO articulo VALUES(null,?,?,?)";
+			String insert="INSERT INTO articulo VALUES (null,?,?,?)";
 			PreparedStatement insertar = (PreparedStatement)connection.prepareStatement(insert);
 			insertarDatos(insertar);
-			break;
+			break;	
 		case 3:
-			String update = "UPDATE articulo SET nombre = ? WHERE categoria=? AND precio = ?";
+			String update = "UPDATE articulo SET categoria =? WHERE nombre= ?";
 			PreparedStatement update1 = (PreparedStatement) connection.prepareStatement(update);
 			updateDatos(update1);
 			break;
 		case 4:
-			String delete = "DELETE FROM articulo WHERE nombre = ?";
+			String delete = "DELETE FROM articulo WHERE id = ?";
 			PreparedStatement delete1 = (PreparedStatement) connection.prepareStatement(delete);
 			deleteDatos(delete1);
-			break;	
+			break;
+		case 5:
+			ResultSet result1 = statement.executeQuery("SELECT * FROM articulo");
+			//mostramos la tabla con los datos
+			mostrarDatos(result1);
+			break;
+		case 0:
+			connection.close();
+			System.out.println("Fin");
 		}
-		ResultSet result1 = statement.executeQuery("SELECT * FROM articulo");
-		//mostramos la tabla con los datos
-		System.out.println("TABLA MODIFICADA:");
-		mostrarDatos(result1);
-		System.out.println("\n");
-
-		connection.close();
-		System.out.println("Fin");
+		}while(num > 0);
 	}
 	
 	private static int insertarDatos(PreparedStatement insertar) throws SQLException{
 		System.out.println("Nombre del articulo :");
-		String nombre = teclado.next();
+		String nombre = teclado.nextLine();
+		insertar.setString(1, nombre);
 		System.out.println("Categoria (1-3):");
 		int categoria = teclado.nextInt();
+		insertar.setInt(2,categoria);
 		System.out.println("Precio:");
 		double precio = teclado.nextDouble();
-		insertar.setString(1, nombre);
-		insertar.setInt(2,categoria);
 		insertar.setDouble(3, precio);
 		return insertar.executeUpdate();
 	}
-	
 	private static int updateDatos(PreparedStatement insertar) throws SQLException{
 		System.out.println("Nombre del articulo :");
 		String nombre = teclado.next();
 		System.out.println("Categoria (1-3):");
 		int categoria = teclado.nextInt();
-		System.out.println("Precio:");
-		double precio = teclado.nextDouble();
 		insertar.setString(1, nombre);
 		insertar.setInt(2, categoria);
-		insertar.setDouble(3, precio);
 		return insertar.executeUpdate();
 	}
 	
 	private static int deleteDatos(PreparedStatement insertar)throws SQLException{
-		System.out.println("Nombre del articulo :");
-		String nombre = teclado.next();
-		insertar.setString(1, nombre);
+		System.out.println("Id del articulo :");
+		int nombre = teclado.nextInt();
+		insertar.setInt(1, nombre);
 		return insertar.executeUpdate();
 	}
 	
